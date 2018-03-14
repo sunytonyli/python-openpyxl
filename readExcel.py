39d4b0a4f10c9bd799a1ac843a25bb38797605d3
sy Li <sy.li>
2018-03-14 08:31:02 +0800

    modify foreach excel

--- readExcel.py
+++ readExcel.py
@@ -5,46 +5,69 @@ from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Align
 import sys
 
 
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-#读到文件
-wb = load_workbook(filename = 'TLW-3月.xlsx', data_only=True)
 
-#根据名称获取工作表
-sheet_ranges = wb['1']
+def reloadExcel(fileExcel):
+    #读到文件
+    wb = load_workbook(filename = fileExcel, data_only=True)
 
-#获取单元格值
-cell_value = sheet_ranges['D18'].value
+    #根据名称获取工作表
+    #sheet_ranges = wb['1']
 
-#循环所有工作表
-for sheet in wb:
-	print(sheet.title)
+    #获取单元格值
+    #cell_value = sheet_ranges['D18'].value
 
-#获取单元格区域
-cell_range = sheet_ranges['B4':'U4']
-for row in cell_range:˝
-	for cell in row:
-		print(cell.value)
+    dataDictionary = {};
+    #循环所有工作表
+    # for sheet in wb:
+    #     print(sheet.title)
 
-#----------------------------华丽的分隔线---------------------------------------
-#读到模板文件
-wb_template = load_workbook(filename = '个人.xlsx', data_only=True)
+    #获取单元格区域
+    # cell_range = sheet_ranges['B4':'U4']
+    # for row in cell_range:
+    #     for cell in row:
+    #         print(cell.value)
 
-#获取工作表
-template_sheet_range = wb_template['Sheet1']
+    for sheet in wb:
+        for row in sheet_ranges['A4':'U55']:
+            dataList = [];
+            for cell in row:
+             dataList.append(cell);
+            pass
 
-#设置单元格值
-template_sheet_range['D8'] = '这是一个测试'
+    copyNewExcel();
 
-#设置工作表名称
-template_sheet_range.title = 'test'
-#复制新的工作表
-copy_sheet_range = wb_template.copy_worksheet(template_sheet_range)
-copy_sheet_range.title = 'copytest'
+def copyNewExcel():
 
-#添加新的工作表
-#ws1 = wb_template.create_sheet('Mysheet')
+    #----------------------------华丽的分隔线---------------------------------------
+    #读到模板文件
+    wb_template = load_workbook(filename = '个人.xlsx', data_only=True)
+
+    #获取工作表
+    template_sheet_range = wb_template['Sheet1']
+
+    #设置单元格值
+    template_sheet_range['D8'] = '这是一个测试'
+
+    #设置工作表名称
+    template_sheet_range.title = 'test'
+    #复制新的工作表
+    copy_sheet_range = wb_template.copy_worksheet(template_sheet_range)
+    copy_sheet_range.title = 'copytest'
+
+    #添加新的工作表
+    #ws1 = wb_template.create_sheet('Mysheet')
+
+    setSheeBoder(template_sheet_range, copy_sheet_range);
+
+    saveExcel(wb_template);
+
+
+
+
 
 #合并单元格样式
 def style_range(ws, cell_range, border=Border(), fill=None, font=None, alignment=None):
@@ -86,29 +109,39 @@ def style_range(ws, cell_range, border=Border(), fill=None, font=None, alignment
             for c in row:
                 c.fill = fill
 
-thin = Side(border_style="thin", color="000000")
-single = Side(border_style="thin", color="000000")
+def setSheeBoder(template_sheet_range, copy_sheet_range):
 
-border = Border(top=single, left=thin, right=thin, bottom=single)
+    thin = Side(border_style="thin", color="000000")
+    single = Side(border_style="thin", color="000000")
 
-#------------------------------------华丽的分隔线------------------------------------------------                
+    border = Border(top=single, left=thin, right=thin, bottom=single)
 
-style_range(template_sheet_range, 'A1:K1', border=border, fill=None, font=None, alignment=None)
-style_range(template_sheet_range, 'A2:D3', border=border, fill=None, font=None, alignment=None)
-style_range(template_sheet_range, 'E2:G3', border=border, fill=None, font=None, alignment=None)
-style_range(template_sheet_range, 'H2:J3', border=border, fill=None, font=None, alignment=None)
-style_range(template_sheet_range, 'K2:K3', border=border, fill=None, font=None, alignment=None)
-style_range(template_sheet_range, 'A4:J5', border=border, fill=None, font=None, alignment=None)
-style_range(template_sheet_range, 'K4:K37', border=border, fill=None, font=None, alignment=None)
+    #------------------------------------华丽的分隔线------------------------------------------------                
 
-style_range(copy_sheet_range, 'A1:K1', border=border, fill=None, font=None, alignment=None)
-style_range(copy_sheet_range, 'A2:D3', border=border, fill=None, font=None, alignment=None)
-style_range(copy_sheet_range, 'E2:G3', border=border, fill=None, font=None, alignment=None)
-style_range(copy_sheet_range, 'H2:J3', border=border, fill=None, font=None, alignment=None)
-style_range(copy_sheet_range, 'K2:K3', border=border, fill=None, font=None, alignment=None)
-style_range(copy_sheet_range, 'A4:J5', border=border, fill=None, font=None, alignment=None)
-style_range(copy_sheet_range, 'K4:K37', border=border, fill=None, font=None, alignment=None)
+    style_range(template_sheet_range, 'A1:K1', border=border, fill=None, font=None, alignment=None)
+    style_range(template_sheet_range, 'A2:D3', border=border, fill=None, font=None, alignment=None)
+    style_range(template_sheet_range, 'E2:G3', border=border, fill=None, font=None, alignment=None)
+    style_range(template_sheet_range, 'H2:J3', border=border, fill=None, font=None, alignment=None)
+    style_range(template_sheet_range, 'K2:K3', border=border, fill=None, font=None, alignment=None)
+    style_range(template_sheet_range, 'A4:J5', border=border, fill=None, font=None, alignment=None)
+    style_range(template_sheet_range, 'K4:K37', border=border, fill=None, font=None, alignment=None)
 
-#保存工作簿 template:是否做为模板
-wb_template.template = False
-wb_template.save('document.xlsx')
+    style_range(copy_sheet_range, 'A1:K1', border=border, fill=None, font=None, alignment=None)
+    style_range(copy_sheet_range, 'A2:D3', border=border, fill=None, font=None, alignment=None)
+    style_range(copy_sheet_range, 'E2:G3', border=border, fill=None, font=None, alignment=None)
+    style_range(copy_sheet_range, 'H2:J3', border=border, fill=None, font=None, alignment=None)
+    style_range(copy_sheet_range, 'K2:K3', border=border, fill=None, font=None, alignment=None)
+    style_range(copy_sheet_range, 'A4:J5', border=border, fill=None, font=None, alignment=None)
+    style_range(copy_sheet_range, 'K4:K37', border=border, fill=None, font=None, alignment=None)
+
+
+def saveExcel(wb_template):
+    #保存工作簿 template:是否做为模板
+    wb_template.template = False
+    wb_template.save('document.xlsx')
+
+def main():
+    print('开始处理EXCEL----------');
+    reloadExcel('TLW-3月.xlsx');
+
+main();
